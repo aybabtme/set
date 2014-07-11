02c71d7c43be0ca011d285d5e84aa548dda79d6c
Antoine Grondin <antoinegrondin@gmail.com>
2014-07-10 20:59:49 -0400

    Organize flags.

--- cmd/compbench/compbench.go
+++ cmd/compbench/compbench.go
@@ -14,6 +14,7 @@ import (
 	"github.com/aybabtme/uniplot/spark"
 	cesparesmaz "github.com/cespare/go-smaz"
 	"github.com/cheggaaa/pb"
+	"github.com/davecheney/profile"
 	"github.com/dustin/go-humanize"
 	inhiessmaz "github.com/inhies/go-smaz"
 	"io"
@@ -25,24 +26,81 @@ import (
 	"time"
 )
 
+type compressor func([]byte) ([]byte, error)
+
+var compressors = map[string]compressor{ // when no match, this is the list printed
+	"mb0-smaz": mb0smaz.Compress,
+
+	"inhies-smaz": noErrorCompressor(inhiessmaz.Compress),
+
+	"cespare-smaz": noErrorCompressor(cesparesmaz.Compress),
+
+	"lzw": writeCloseCompressor(func(buf *bytes.Buffer) io.WriteCloser {
+		return lzw.NewWriter(buf, lzw.LSB, 8)
+	}),
+
+	"gzip": writeCloseCompressor(func(buf *bytes.Buffer) io.WriteCloser {
+		return gzip.NewWriter(buf)
+	}),
+
+	"flate": writeCloseCompressor(func(buf *bytes.Buffer) io.WriteCloser {
+		w, err := flate.NewWriter(buf, flate.DefaultCompression)
+		if err != nil {
+			log.Panic(err)
+		}
+		return w
+	}),
+
+	"zlib": writeCloseCompressor(func(buf *bytes.Buffer) io.WriteCloser {
+		return zlib.NewWriter(buf)
+	}),
+}
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	log.SetPrefix("[compbench] ")
 	log.SetFlags(0)
 	var (
-		compression = flag.String("comp", "lzw", "compression algorithm to benchmark")
+		compression = flag.String("comp", "", "compression algorithm to benchmark")
+		cpuprof     = flag.Bool("profcpu", false, "whether to run a cpu profiler")
+		memprof     = flag.Bool("profmem", false, "whether to run a memory profiler, ignored if cpuprof is set")
+		blockprof   = flag.Bool("profblock", false, "whether to run a block profiler, ignored if one of cpuprof/memprof is set")
 	)
+	flag.Usage = func() {
+		fmt.Println(`USAGE: compbench [flags] filename`)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 	flag.Parse()
 
+	if *cpuprof {
+		defer profile.Start(profile.CPUProfile).Stop()
+	} else if *memprof {
+		defer profile.Start(profile.MemProfile).Stop()
+	} else if *blockprof {
+		defer profile.Start(profile.MemProfile).Stop()
+	}
+
 	if len(flag.Args()) != 1 {
-		log.Fatalf("need a unique argument (filename)")
+		fmt.Println("need at least a filename")
+		flag.Usage()
 	}
 
 	filename := flag.Args()[0]
 
+	compress, ok := compressors[*compression]
+	if !ok {
+		fmt.Printf("no compression algorithm named %q, algorithms are:\n", *compression)
+		for k := range compressors {
+			fmt.Printf("\t- %s\n", k)
+		}
+		flag.Usage()
+	}
+
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Fatalf("opening %q: %v", filename, err)
+		fmt.Printf("opening %q: %v", filename, err)
+		flag.Usage()
 	}
 	defer file.Close()
 
@@ -58,11 +116,6 @@ func main() {
 		log.Fatalf("scanning lines in %q: %v", filename, err)
 	}
 
-	compress, err := selectCompressor(*compression)
-	if err != nil {
-		log.Fatalf("not a compressor, %q: %v", *compression, err)
-	}
-
 	log.Printf("compressing %d lines...", len(lines))
 	report, err := compressLines(lines, compress)
 	if err != nil {
@@ -70,47 +123,6 @@ func main() {
 	}
 	printStats(report)
 
-}
-
-type compressor func([]byte) ([]byte, error)
-
-func selectCompressor(name string) (compressor, error) {
-	switch name {
-	case "lzw":
-		return writeCloseCompressor(func(buf *bytes.Buffer) io.WriteCloser {
-			return lzw.NewWriter(buf, lzw.LSB, 8)
-		}), nil
-
-	case "gzip":
-		return writeCloseCompressor(func(buf *bytes.Buffer) io.WriteCloser {
-			return gzip.NewWriter(buf)
-		}), nil
-	case "flate":
-		return writeCloseCompressor(func(buf *bytes.Buffer) io.WriteCloser {
-			w, err := flate.NewWriter(buf, flate.DefaultCompression)
-			if err != nil {
-				log.Panic(err)
-			}
-			return w
-		}), nil
-
-	case "zlib":
-		return writeCloseCompressor(func(buf *bytes.Buffer) io.WriteCloser {
-			return zlib.NewWriter(buf)
-		}), nil
-
-	case "mb0-smaz":
-		return mb0smaz.Compress, nil
-
-	case "inhies-smaz":
-		return noErrorCompressor(inhiessmaz.Compress), nil
-
-	case "cespare-smaz":
-		return noErrorCompressor(cesparesmaz.Compress), nil
-
-	default:
-		return nil, fmt.Errorf("bad compressor type: %q", name)
-	}
 }
 
 func compressLines(lines [][]byte, compress compressor) ([]Report, error) {
@@ -175,11 +187,11 @@ func printStats(reps []Report) {
 	byRatio := reportByRatio(reps)
 	sort.Sort(byRatio)
 
-	log.Printf("By precompression size")
+	log.Printf("Distribution of sizes before compression")
 	err := histogram.Fprintf(
 		os.Stdout,
 		histogram.Hist(10, byRatio.Froms()),
-		histogram.Linear(40),
+		histogram.Linear(20),
 		func(v float64) string {
 			return humanize.Bytes(uint64(v))
 		})
@@ -187,11 +199,11 @@ func printStats(reps []Report) {
 		log.Fatalf("plotting by ratio: %v", err)
 	}
 
-	log.Printf("By compression size")
+	log.Printf("Distribution of sizes after compression")
 	err = histogram.Fprintf(
 		os.Stdout,
-		histogram.Hist(10, Significant(byRatio.Tos())),
-		histogram.Linear(40),
+		histogram.Hist(10, byRatio.Tos()),
+		histogram.Linear(20),
 		func(v float64) string {
 			return humanize.Bytes(uint64(v))
 		})
@@ -202,10 +214,10 @@ func printStats(reps []Report) {
 	log.Printf("By compression ratio")
 	err = histogram.Fprintf(
 		os.Stdout,
-		histogram.Hist(10, Significant(byRatio.Ratios())),
-		histogram.Linear(40),
+		histogram.Hist(10, byRatio.Ratios()),
+		histogram.Linear(20),
 		func(v float64) string {
-			return fmt.Sprintf("%.1g%%", v*100.0)
+			return fmt.Sprintf("%.1f%%", v*100.0)
 		})
 	if err != nil {
 		log.Fatalf("plotting by ratio: %v", err)
@@ -215,7 +227,7 @@ func printStats(reps []Report) {
 	err = histogram.Fprintf(
 		os.Stdout,
 		histogram.Hist(10, byRatio.SavedBytes()),
-		histogram.Linear(40),
+		histogram.Linear(20),
 		func(v float64) string {
 			return humanize.Bytes(uint64(v))
 		})
@@ -229,7 +241,7 @@ func printStats(reps []Report) {
 	err = histogram.Fprintf(
 		os.Stdout,
 		histogram.Hist(10, Significant(byTime.Times())),
-		histogram.Linear(40),
+		histogram.Linear(20),
 		func(v float64) string {
 			return time.Duration(v).String()
 		})
